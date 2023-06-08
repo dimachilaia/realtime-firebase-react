ebfe593d66a5a93cb0821c2c5e4dbaee3869cc3b
dima <chilaiadima@Gmail.com>
2023-06-08 13:32:45 +0400

    created chat component

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -1,32 +1,85 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import {
   addDoc,
   collection,
   serverTimestamp,
   onSnapshot,
   query,
-  where,
+  orderBy,
 } from "firebase/firestore";
 import { db, auth } from "../firebaseConfig";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import styled from "styled-components";
+
+const ChatContainer = styled(Container)`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ChatWindow = styled.div`
+  border: 1px solid #ccc;
+  width: 80%;
+  height: 60vh;
+  overflow-y: auto;
+  padding: 10px;
+`;
+
+const MessageInputContainer = styled(Form)`
+  width: 80%;
+  padding: 10px;
+  margin-top: 10px;
+  border: 1px solid #ccc;
+  border-top: none;
+`;
+
+const Message = styled.div<{ isCurrentUser: boolean }>`
+  display: flex;
+  justify-content: ${(props) =>
+    props.isCurrentUser ? "flex-end" : "flex-start"};
+  align-items: flex-start;
+  margin-bottom: 10px;
+`;
+
+const MessageContent = styled.div<{ isCurrentUser: boolean }>`
+  padding: 10px;
+  border-radius: 5px;
+  max-width: 60%;
+  word-break: break-word;
+  background-color: ${(props) => (props.isCurrentUser ? "#d4e6ff" : "#f0f0f0")};
+`;
+
+const MessageUser = styled.div`
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
 
 const Chat = () => {
-  const [newMessage, setNewMessage] = useState<any>("");
-  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState<any[]>([]);
+  const chatWindowRef = useRef<HTMLDivElement>(null);
 
   const messagesRef = collection(db, "messages");
 
   useEffect(() => {
-    const queryMessages = query(messagesRef);
+    const queryMessages = query(messagesRef, orderBy("createdAt", "desc"));
     const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
-      let messages: any = [];
+      const messages: any[] = [];
       snapshot.forEach((doc) => {
         messages.push({ ...doc.data(), id: doc.id });
       });
-      setMessages(messages);
+      setMessages(messages.reverse());
     });
     return () => unsubscribe();
   }, []);
+
+  useEffect(() => {
+    if (chatWindowRef.current) {
+      chatWindowRef.current.scrollTop = chatWindowRef.current.scrollHeight;
+    }
+  }, [messages]);
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
@@ -34,44 +87,43 @@ const Chat = () => {
     await addDoc(messagesRef, {
       text: newMessage,
       createdAt: serverTimestamp(),
-      user: auth.currentUser?.displayName,
+      user: auth.currentUser?.displayName || "Unknown User",
     });
     setNewMessage("");
   };
 
   return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <div className="chat-window">
-            <div className="chat-messages">
-              {messages.map((message: any) => (
-                <div
-                  key={message.id}
-                  className={`message ${
-                    message.user === "user" ? "user" : "bot"
-                  }`}
+    <ChatContainer>
+      <ChatWindow ref={chatWindowRef}>
+        <div className="chat-messages">
+          {auth.currentUser &&
+            messages.map((message) => (
+              <Message
+                key={message.id}
+                isCurrentUser={message.user === auth.currentUser?.displayName}
+              >
+                <MessageContent
+                  isCurrentUser={message.user === auth.currentUser?.displayName}
                 >
-                  <strong>{message.user}: </strong>
+                  <MessageUser>{message.user}</MessageUser>
                   {message.text}
-                </div>
-              ))}
-            </div>
-            <Form className="chat-input" onSubmit={handleSubmit}>
-              <Form.Control
-                type="text"
-                placeholder="Type your message"
-                value={newMessage}
-                onChange={(e) => setNewMessage(e.target.value)}
-              />
-              <Button variant="primary" type="submit">
-                Send
-              </Button>
-            </Form>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+                </MessageContent>
+              </Message>
+            ))}
+        </div>
+      </ChatWindow>
+      <MessageInputContainer onSubmit={handleSubmit}>
+        <Form.Control
+          type="text"
+          placeholder="Type your message"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+        />
+        <Button variant="primary mt-3" type="submit" onClick={handleSubmit}>
+          Send
+        </Button>
+      </MessageInputContainer>
+    </ChatContainer>
   );
 };
 
